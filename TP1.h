317bcc1a54a33159dec5e7ea5f1c92ece7c26452
william2305-d <william2305@hotmail.com>
2018-10-30 10:21:08 -0300

    Inicialização de matriz ainda não completa

--- TP1.h
+++ TP1.h
@@ -4,10 +4,12 @@ using namespace std;
 class Matriz{
 	private:
 	public:
+		double **p;
 		Matriz();
 		Matriz(const Matriz &m);
 		Matriz(int linhas, int colunas, const double &valor = 0);
 		~Matriz();
+		void imprimeMatriz(Matriz &m);
 		
 };
 
@@ -15,10 +17,22 @@ Matriz::Matriz(const Matriz &m){
 }
 
 Matriz::Matriz(){
+	p = new double * [1];
+	p[1] = 0;
 }
 
 Matriz::Matriz(int linhas, int colunas, const double &valor){
+	p = new double * [colunas];
+	for(int i = 0; i<linhas; i++){
+		p[i] = new double[linhas];
+		for(int j = 0; j<colunas; j++){
+			p[i][j] = 0;
+		}
+	}
 }
 
 Matriz::~Matriz(){
+}
+
+Matriz::imprimeMatriz(Matriz &m){
 }
