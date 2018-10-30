714897ddd4e933a1639e6303dff9ab9bc8d9db8a
william2305-d <william2305@hotmail.com>
2018-10-29 22:10:44 -0300

    Criação do arquivo TP1.h

---
+++ TP1.h
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//criando a classe
+class Matriz{
+	private:
+	public:
+		Matriz();
+		Matriz(const Matriz &m);
+		Matriz(int linhas, int colunas, const double &valor = 0);
+		~Matriz();
+		
+};
+
+Matriz::Matriz(const Matriz &m){
+
+}
+
+Matriz::Matriz(){
+}
+
+Matriz::Matriz(int linhas, int colunas, const double &valor){
+}
+
+Matriz::~Matriz(){
+}
