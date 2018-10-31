aa123e83e30c2635d34d5c2db31813688e3faddc
william2305-d <william2305@hotmail.com>
2018-10-31 12:49:51 -0300

    implementação das funções no arquivo Matriz.cpp

--- Matriz.h
+++ Matriz.h
@@ -14,29 +14,3 @@ class Matriz{
 		void imprimeMatriz();
 		
 };
-
-Matriz::Matriz(const Matriz &m){
-}
-
-Matriz::Matriz(){
-	p = new double * [1];
-	p[1] = 0;
-}
-
-Matriz::Matriz(int linhas, int colunas, const double &valor){
-	l = linhas;
-	c = c;
-	p = new double * [c];
-	for(int i = 0; i<l; i++){
-		p[i] = new double[l];
-		for(int j = 0; j<c; j++){
-			p[i][j] = 0;
-		}
-	}
-}
-
-Matriz::~Matriz(){
-}
-
-void imprimeMatriz(){
-}
