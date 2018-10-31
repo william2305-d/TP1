0f7683731349fd10c78d63f84b0f57dab4990942
william2305-d <william2305@hotmail.com>
2018-10-31 10:55:28 -0300

    mudança de nome para Matriz.h

---
+++ Matriz.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "TP1.h"
+
+using namespace std;
+
+using namespace std;
+int main(){
+	Matriz M;
+	cout << M.p[1] << endl;
+	return(0);
+}
---
+++ Matriz.h
@@ -0,0 +1,42 @@
+#include <iostream>
+//modificação
+using namespace std;
+//criando a classe
+class Matriz{
+	private:
+		int l,c;
+	public:
+		double **p;
+		Matriz();
+		Matriz(const Matriz &m);
+		Matriz(int linhas, int colunas, const double &valor = 0);
+		~Matriz();
+		void imprimeMatriz();
+		
+};
+
+Matriz::Matriz(const Matriz &m){
+}
+
+Matriz::Matriz(){
+	p = new double * [1];
+	p[1] = 0;
+}
+
+Matriz::Matriz(int linhas, int colunas, const double &valor){
+	l = linhas;
+	c = c;
+	p = new double * [c];
+	for(int i = 0; i<l; i++){
+		p[i] = new double[l];
+		for(int j = 0; j<c; j++){
+			p[i][j] = 0;
+		}
+	}
+}
+
+Matriz::~Matriz(){
+}
+
+void imprimeMatriz(){
+}
