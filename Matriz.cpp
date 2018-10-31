950acadd34e52fc1d1f262411a9671866b01770c
william2305-d <william2305@hotmail.com>
2018-10-31 12:49:04 -0300

    criada função para imprimir matriz e inicialização de matriz

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
-#include "TP1.h"
+#include "Matriz.h"
 
 using namespace std;
+Matriz::Matriz(const Matriz &m){
 
-using namespace std;
+}
+
+Matriz::Matriz(){
+	p = new double *[1];
+	p[1] = 0;
+}
+
+Matriz::Matriz(int linhas, int colunas, const double &valor){
+	l = linhas;
+	c = colunas;
+	p = new double * [l];
+	for(int i = 0; i<l; i++){
+		p[i] = new double[c];
+		for(int j = 0; j<c; j++){
+			p[i][j] = 0;
+		}
+	}
+}
+
+Matriz::~Matriz(){
+
+}
+
+void Matriz::imprimeMatriz(){
+	for(int i = 0; i<l ; i++){
+		for(int j = 0; j<c; j++){
+			cout << p[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 int main(){
-	Matriz M;
-	cout << M.p[1] << endl;
+	Matriz M(5,3,0);
+	M.imprimeMatriz();
 	return(0);
 }
