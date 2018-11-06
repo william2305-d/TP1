cd492e89a8cd0ce8d9c1e3b99a692b7a6a4981f5
manu4566 <manu4566@gmail.com>
2018-11-06 10:37:13 -0200

    Completo

--- Matriz.h
+++ Matriz.h
@@ -1,5 +1,4 @@
 #include <iostream>
-//modifica√ß√£o
 using namespace std;
 //criando a classe
 class Matriz{
@@ -11,9 +10,26 @@ class Matriz{
 		Matriz(const Matriz &m);
 		Matriz(int linhas, int colunas, const double &valor = 0);
 		~Matriz();
-		void imprimeMatriz();
 		int zeros();
 		int getRows();
 		int getCols();
 		int ones();
+		Matriz operator+(const Matriz &A)const;
+		void operator+=(const Matriz &A); //n„o sao constante pq altera a primeira Matriz
+		void operator~();
+		//Caio
+		void operator*=(double const &right);
+		Matriz operator*(double const &right);
+		Matriz operator*(const Matriz &right);
+		void operator*=(const Matriz &right);
+		friend ostream& operator<< (ostream& os, const Matriz& m); //contante tambÈm, apenas leitura
+		//Manu
+		Matriz operator-(const Matriz &A)const;
+		void operator-=(const Matriz &A);
+		friend istream& operator>>(istream& os, Matriz& m);
+		void operator=(const Matriz &A);
+		int operator==(const Matriz &A)const;
+		int operator!=(const Matriz &A)const;
+		double& operator()(int const &linhas, int const &colunas); 
+		
 };
